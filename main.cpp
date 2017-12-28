e80f9656272694b9e23be24c52665f39ccdc1908
hydra <nguyentrungduc08@gmail.com>
2017-12-28 09:42:08 +0700

    initialize code

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: hydra
+ *
+ * Created on December 27, 2017, 2:35 PM
+ */
+
+#include <cstdlib>
+#include <boost/make_shared.hpp>
+//#include "libtorrent/include/libtorrent/entry.hpp"
+//#include "libtorrent/include/libtorrent/bencode.hpp"
+//#include "libtorrent/include/libtorrent/session.hpp"
+//#include "libtorrent/include/libtorrent/torrent_info.hpp"
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
