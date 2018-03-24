a6b7edfaa8f728b1d4470bb8b679cb6e4f59f52e
hydra <nguyentrungduc08@gmail.com>
2018-03-24 21:20:04 +0700

    add hash table struct

--- header/Hasing.h
+++ header/Hasing.h
@@ -14,7 +14,45 @@
 #ifndef HASING_H
 #define HASING_H
 
+#include "fileserver.h"
 
+const int BUCKETS = 1e6 + 3;
+
+template <class Type>
+struct HashTable {
+    std::vector< std::pair<Type,Type> > h[P];
+
+public:
+    void insert(Type key, Type value) {
+        int hkey = getHash(key);
+        for (auto p : h[hkey]) {
+            if (p.first == key) {
+                
+                return;
+            }
+        }
+
+        h[hkey].emplace_back(key, value);
+    }
+
+    int find(Type key) {
+        int hkey = getHash(key);
+        for(auto p : h[hkey]) {
+            if (p.first == key) {
+                
+                return p.value;
+            }
+        }
+        
+        return 0;
+    }
+
+private:
+    int getHash(Type key) {
+        
+        return key % P;
+    }
+};
 
 #endif /* HASING_H */
 
